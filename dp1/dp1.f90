89297019b561ca081718bfb3e13f84fcdda4c734
Pahoomich <moshguuy@gmail.com>
2018-03-17 15:35:44 +0700

    Добавьте файлы проекта.

---
+++ dp1/dp1.f90
@@ -0,0 +1,25 @@
+!  dp1.f90 
+!
+!  FUNCTIONS:
+!  dp1 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: dp1
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program dp1
+
+    implicit none
+
+    ! Variables
+
+    ! Body of dp1
+    print *, 'Hello World'
+
+    end program dp1
+
